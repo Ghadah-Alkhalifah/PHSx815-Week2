30b4e7737909a78f7605f895763540985e2abcca
Ghadah-Alkhalifah <78434228+Ghadah-Alkhalifah@users.noreply.github.com>
2023-01-27 11:48:00 -0600

    Hw3 plot code

---
+++ HW3-box-m.py
@@ -0,0 +1,34 @@
+from Random_HW3 import Random
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    
+    seed=5555
+    randomNum = Random(seed)
+    y1 =[]
+    for i in range(0,10000):
+        y1.append(randomNum.BoxM())
+    #plot histogram    
+    n, bins, patches = plt.hist(y1, 50, density=True, facecolor='red', alpha=0.75, ec='black')
+    # plot formating options
+    plt.xlabel('y1')
+    plt.ylabel('Probability')
+    plt.title('Box-Muller transform-1')
+    plt.grid(True)
+    plt.savefig('plot1.pdf')
+    # show figure (program only ends once closed
+    plt.show()
+
+    #plot histogram 
+    
+    y2 =[]
+    for i in range(0,10000):
+        y2.append(randomNum.BoxM())
+    n, bins, patches = plt.hist(y2, 50, density=True, facecolor='blue', alpha=0.75, ec='black')
+    plt.xlabel('y2')
+    plt.ylabel('Probability')
+    plt.title('Box-Muller transform-2')
+    plt.grid(True)
+    plt.savefig('plot2.pdf')
+    plt.show()
